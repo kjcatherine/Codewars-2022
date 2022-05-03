348243c16622ca336bc1a6db0714c1b082c19a4f
kate <katejohn967@gmail.com>
2022-05-03 01:13:19 +0100

    solution 14

---
+++ js/solution014.js
@@ -0,0 +1,12 @@
+//14. We need a function that can transform a number into a string. What ways of achieving this do you know
+//a.
+function numberToString(num) {
+  return num.toString();
+}
+numberToString(20);
+
+//b.
+function numberToString(num) {
+  let strNum = num + "";
+  return strNum;
+}
