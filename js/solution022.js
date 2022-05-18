6e0a44c1a32becba354413830a525a8d695ffbad
kate <katejohn967@gmail.com>
2022-05-18 13:36:30 +0100

    edited code block

--- js/solution022.js
+++ js/solution022.js
@@ -1,22 +1,29 @@
 "use strict";
 
 //22
-//Given an array of integers.Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative. If the input is an empty array or is null, return an empty array.
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+// If the input array is empty or null, return an empty array.
 
 function countPositivesSumNegatives(input) {
-  let result = [];
-  const positives = input.filter((int, i) => int > 0);
-  console.log(positives);
-  //.reduce((acc, int) => acc + int, 0);
-  // const negatives = input
-  //   .filter((int) => int < 0)
-  //   .reduce((acc, int) => acc + int, 0);
-  // result.push(positives, negatives);
-  // console.log(result);
-  //console.log(positives, negatives);
-  //console.log(input.sort((a, b) => a - b));
+  let positives = 0;
+  let negatives = 0;
+  if (input === null || input.length === 0) {
+    return [];
+  } else {
+    input.forEach((num) => (num > 0 ? positives++ : (negatives += num)));
+  }
+  return [positives, negatives];
 }
-countPositivesSumNegatives([
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
-]);
-// you should return [10, -65].
+countPositivesSumNegatives([1, 2, 3, 4, 5, -12, -2, -23, -60]);
+
+//HOW I SOLVED THIS
+//1. I set two variables, positiveNums and negativeNums, to 0.
+
+//2. I then proceeded to check the condition of the challenge first,(if the input is null or empty)
+
+//3. Where input is neither empty, nor null, i checked if the elements in the array are greater than zero, ran a connditional to count the positives and proceeded to add the negative numbers together.I used a forEach method to achieve this.
+
+//4. To finish up, I returned the result of the two variables in an array
