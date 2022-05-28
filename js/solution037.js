218db73f4c69315444980f814791afc2deb9a329
kate <katejohn967@gmail.com>
2022-05-29 00:23:41 +0100

    solution to are you playing banjo kata

---
+++ js/solution037.js
@@ -0,0 +1,18 @@
+//ARE YOU PLAYING BANJO- Create a function which answers the question "Are you playing banjo?".If your name starts with the letter "R" or lower case "r", you are playing banjo!The function takes a name as its only argument, and returns one of the following strings:
+
+function areYouPlayingBanjo(name) {
+  if (name.startsWith("r") || name.startsWith("r".toUpperCase())) {
+    return `${name} plays banjo`;
+  } else {
+    return `${name} does not play banjo`;
+  }
+}
+
+//solution2
+// function areYouPlayingBanjo(name) {
+// if (name[0] === 'R' || name[0] === 'r'){
+//   return `${name} plays banjo`
+// }else{
+//  return `${name} does not play banjo`
+// }
+// }
