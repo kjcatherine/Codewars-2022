7601910f7ade07b6aba865b2e80f2681127976b7
kate <katejohn967@gmail.com>
2022-05-30 00:04:28 +0100

    made it better

--- js/solution036.js
+++ js/solution036.js
@@ -1,1 +1,6 @@
-return a.indexOf(x) > -1 ? true : false;
+//You only need one_ You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.Array can contain numbers or strings. X can be either.Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  // your code here
+  return a.indexOf(x) > -1 ? true : false;
+}
