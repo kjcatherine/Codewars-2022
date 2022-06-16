af2f92fbdc8f9b1ca375e95009c897d59145472d
kate <katejohn967@gmail.com>
2022-06-16 10:26:19 +0100

    sum of two lowest positive integers kata

---
+++ js/solution057.js
@@ -0,0 +1,9 @@
+//SUM OF TWO LOWEST POSITIVE INTEGERS- Create a function that returns the sum
+//of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7
+
+function sumTwoSmallestNumbers(numbers) {
+  const sorted = numbers.sort((a, b) => a - b);
+  return sorted[0] + sorted[1];
+}
+
+sumTwoSmallestNumbers([10, 123, 567, 2, 90]);
