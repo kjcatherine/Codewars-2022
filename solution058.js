f82bb2ebb6eae3f2918731f11a1f2e69c58245ea
kate <katejohn967@gmail.com>
2022-06-17 12:56:37 +0100

    Beginner Series #3 Sum of Numbers kata

---
+++ solution058.js
@@ -0,0 +1,17 @@
+//Beginner Series #3 Sum of Numbers- Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+//SOLUTION
+function getSum(a, b) {
+  let count = 0;
+  if (a == b) return a;
+  if (a < b) {
+    for (; a <= b; a++) {
+      count += a;
+    }
+  } else if (a > b) {
+    for (; b <= a; b++) {
+      count += b;
+    }
+  }
+  return count;
+}
