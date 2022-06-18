524700f958f4ef0531ed18028d14dda3aa16e180
kate <katejohn967@gmail.com>
2022-06-18 13:10:26 +0100

    credit card mask kata

---
+++ js/solution059.js
@@ -0,0 +1,11 @@
+//CREDIT CARD MASK- Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+//Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+//SOLUTION
+function maskify(cc) {
+  const str = cc + "";
+  const last4 = str.slice(-4);
+  console.log(last4.padStart(str.length, "#"));
+}
+maskify(4556364607935616);
